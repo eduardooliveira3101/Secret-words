649970dc70f7598a3655a1f5ae6a22e96b4e2a2e
Carlos Eduardo <107800809+eduardooliveira3101@users.noreply.github.com>
2025-03-17 14:01:47 -0300

    Secret-Words

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {StartScreen} from './components/StartScreen'
+import styles from './App.module.css'
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <StartScreen/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import '../index.css'
+import styles from './StartScreen.module.css'
+
+export function StartScreen() {
+  return (
+    <div>
+      <h1>Secret Words</h1>
+      <p>Clique no botão abaixo para começar</p>
+      <button className={styles.button}>Começar jogo</button>
+    </div>
+  )
+}
