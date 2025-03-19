277a807bde709006bff236f40d06b7671d42447c
Carlos Eduardo <c.edu31@hotmail.com>
2025-03-19 16:31:18 -0300

    game display

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-
-import '../index.css'
-// eslint-disable-next-line no-unused-vars
 import styles from './Game.module.css'
+import '../index.css'
 
-export function Game({verifyLetter}) {
+export function Game() {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Adivinhar letra</button>
+    <div className={styles.game}>
+      <p className={styles.points} >
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Adivinhe a palavra: </h1>
+      <h3 className={styles.tip}>
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+
+      <div className={styles.wordContainer}>
+        <span className={styles.letter}>A</span>
+        <span className={styles.blankSquare}></span>
+      </div>
+
+      <div className={styles.letterContainer}>
+        <p>Tente adivinhar uma letra da palavra: </p>
+        <form>
+          <input type="text" name='letter' maxLength={1} required />
+          <button>Jogar</button>
+        </form>
+      </div>
+
+      <div className={styles.wrongLettersContainer}>
+        <p>Letras já utilizadas</p>
+        <span>a, </span>
+        <span>b, </span>
+        <span>c, </span>
+      </div>
     </div>
   )
 }
